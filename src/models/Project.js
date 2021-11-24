38b1c3f9a25f331ec2b01c2b8fe01af3a3ada2df
RenanBartalo <renan.bartalo@outlook.com>
2021-11-23 21:48:57 -0300

    adding models

--- src/models/Project.js
+++ src/models/Project.js
@@ -7,6 +7,18 @@ const projectSchema = new Schema({
   description: {
     type: String, required: true, minlength: 15, maxlength: 150,
   },
+  cidade: {
+    type: String, minlength: 3, maxlength: 30,
+  },
+  dataDeIda: {
+    type: Date,
+  },
+  dataDeVolta: {
+    type: Date,
+  },
+  selectedFile: {
+    type: String,
+  },
   tasks: [{ type: Schema.Types.ObjectId, ref: 'task', default: [] }], // One To Many
   owner: { type: Schema.Types.ObjectId, ref: 'user', required: true }, // One To One
 }, {
