f00e6f8c3011e4fa44e6deb919b0228db2ec7d02
RenanBartalo <renan.bartalo@outlook.com>
2021-11-25 19:55:57 -0300

    adding day Service

---
+++ src/service/daysService.js
@@ -0,0 +1,59 @@
+/* eslint-disable class-methods-use-this */
+import * as yup from 'yup';
+import mongoose from 'mongoose';
+
+import InvalidBodyRequestException from '../exceptions/InvalidBodyRequestException';
+import InvalidIdException from '../exceptions/InvalidIdException';
+
+class DaysService {
+  constructor(dayRepository, travelRepository) {
+    this.dayRepository = dayRepository;
+    this.travelRepository = travelRepository;
+  }
+
+  async getDaysByTravelId(travelId) {
+    const isIdValid = mongoose.isValidObjectId(travelId);
+
+    if (!isIdValid) {
+      throw new InvalidIdException();
+    }
+
+    const days = await this.dayRepository.getDaysByTravelId(travelId);
+
+    return days;
+  }
+
+  async create(body, travelId) {
+    const schema = yup.object().shape({
+      title: yup.string().required('Required Field').min(6, 'Minimum of 6 characters').max(50, 'Maximum of 50 characters'),
+      description: yup.string().required('Required Field').min(15, 'Minimum of 15 characters').max(150, 'Maximum of 150 characters'),
+    });
+
+    try {
+      await schema.validate(body, { abortEarly: false });
+    } catch (error) {
+      const errors = error.inner.map((err) => ({
+        field: err.path,
+        error: err.errors.length > 0 ? err.errors : err.errors[0],
+      }));
+
+      throw new InvalidBodyRequestException(errors);
+    }
+
+    const isIdValid = mongoose.isValidObjectId(travelId);
+
+    if (!isIdValid) {
+      throw new InvalidIdException();
+    }
+
+    const savedDay = await this.dayRepository.createNewDay({ ...body, travel: travelId });
+
+    // Aqui em baixo temos acesso ao _id da nova task
+    // Precisa pegar o projeto pelo ID e inserir dentro de tasks o ID da tarefa criada acima
+    await this.travelRepository.insertDayId(travelId, savedDay._id);
+
+    return savedDay;
+  }
+}
+
+export default DaysService;
